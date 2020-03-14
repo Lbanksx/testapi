50368dcbc7376b8a3c13ceb43547aa0d26357642
HugoNSantos <hugonsantos03@gmail.com>
2020-03-14 19:38:45 -0300

    Product

--- src/main/java/com/hugonsantos/testapi/config/TestConfig.java
+++ src/main/java/com/hugonsantos/testapi/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.hugonsantos.testapi.entities.Category;
 import com.hugonsantos.testapi.entities.Order;
+import com.hugonsantos.testapi.entities.Product;
 import com.hugonsantos.testapi.entities.User;
 import com.hugonsantos.testapi.entities.enums.OrderStatus;
 import com.hugonsantos.testapi.repositories.CategoryRepository;
 import com.hugonsantos.testapi.repositories.OrderRepository;
+import com.hugonsantos.testapi.repositories.ProductRepository;
 import com.hugonsantos.testapi.repositories.UserRepository;
 
 @Configuration
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	public TestConfig() {
 			
 	}
@@ -40,6 +45,12 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Lorem ipsum dolor sit amet, consectetur", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Lorem ipsum dolor sit amet, consectetur", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Lorem ipsum dolor sit amet, consectetur", 1200.9, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Lorem ipsum dolor sit amet, consectetur", 100.99, "");
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Hugo Nascimento", "hugonsantos03@gmail.com", "948523236", "Hugo100");
 		
@@ -47,6 +58,7 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
