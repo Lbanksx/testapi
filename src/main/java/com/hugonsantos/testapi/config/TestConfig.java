cc67c15f26bc1a61ff59967a11e7fb41815b2477
HugoNSantos <hugonsantos03@gmail.com>
2020-03-14 19:59:31 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/hugonsantos/testapi/config/TestConfig.java
+++ src/main/java/com/hugonsantos/testapi/config/TestConfig.java
@@ -51,6 +51,18 @@ public class TestConfig implements CommandLineRunner{
 		Product p4 = new Product(null, "PC Gamer", "Lorem ipsum dolor sit amet, consectetur", 1200.9, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Lorem ipsum dolor sit amet, consectetur", 100.99, "");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Hugo Nascimento", "hugonsantos03@gmail.com", "948523236", "Hugo100");
 		
@@ -58,8 +70,6 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
