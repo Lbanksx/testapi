1c80344dc06637645b1e4127a16b2eb6f40c6cf2
HugoNSantos <hugonsantos03@gmail.com>
2020-03-14 17:32:37 -0300

    Service layer and component registration

---
+++ src/main/java/com/hugonsantos/testapi/services/UserService.java
@@ -0,0 +1,26 @@
+package com.hugonsantos.testapi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hugonsantos.testapi.entities.User;
+import com.hugonsantos.testapi.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
