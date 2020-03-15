ac56e5d58136795c4692435cc7b85d05fe297b19
HugoNSantos <hugonsantos03@gmail.com>
2020-03-15 11:21:14 -0300

    Payment and one-to-one association

--- src/main/java/com/hugonsantos/testapi/config/TestConfig.java
+++ src/main/java/com/hugonsantos/testapi/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.hugonsantos.testapi.entities.Category;
 import com.hugonsantos.testapi.entities.Order;
 import com.hugonsantos.testapi.entities.OrderItem;
+import com.hugonsantos.testapi.entities.Payment;
 import com.hugonsantos.testapi.entities.Product;
 import com.hugonsantos.testapi.entities.User;
 import com.hugonsantos.testapi.entities.enums.OrderStatus;
@@ -84,5 +85,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
